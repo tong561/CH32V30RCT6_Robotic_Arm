f0944385039fb0942eabd0a1ab6430eea8279406
worth-moon <129928237+worth-moon@users.noreply.github.com>
2025-08-28 16:20:39 +0800

    添加步进电机控制api的外部声明

--- CAN/Networking/Architecture/module/STEP/step.h
+++ CAN/Networking/Architecture/module/STEP/step.h
@@ -5,6 +5,7 @@
 #include "stdint.h"
 
 u8 CAN_Send_Position_Mode(u32 address, u8 direction, u16 speed, u8 acceleration, u32 pulse_count, u8 position_mode, u8 sync_mode);
+void Control_Motor(float angle, u8 id);
 #endif
 
 
